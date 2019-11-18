b3ef46b2ba83f9d5a0017491d4bbfcb4af08429f
bojanfriscic <friscic.bojan@gmail.com>
2019-11-18 16:38:34 +0100

    Styles added

--- src/components/NewPaletteForm/NewPaletteForm.styles.js
+++ src/components/NewPaletteForm/NewPaletteForm.styles.js
@@ -1,24 +1,48 @@
 const drawerWidth = 400;
 
-const styles = {
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    padding: '1.5rem',
-    marginLeft: -drawerWidth,
-    transition: 'all 300ms ease-in-out',
+const styles = theme => ({
+  root: {
+    display: 'flex',
   },
 
-  contentShift: {
-    marginLeft: 0,
-    transition: 'all 300ms ease-in-out',
+  drawer: {
+    flexShrink: 0,
+    width: drawerWidth,
+    height: '100vh',
+  },
+
+  drawerPaper: {
+    display: 'flex',
+    alignItems: 'center',
+    width: drawerWidth,
   },
 
   drawerHeader: {
     display: 'flex',
     alignItems: 'center',
-    padding: '0 .5rem',
     justifyContent: 'flex-end',
+    width: '100%',
+    padding: '0 .5rem',
+    ...theme.mixins.toolbar,
+  },
+
+  content: {
+    flexGrow: 1,
+    height: 'calc(100vh - 64px)',
+    padding: '1.5rem',
+    marginLeft: -drawerWidth,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+
+  contentShift: {
+    marginLeft: 0,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
 
   boxContainer: {
@@ -27,6 +51,14 @@ const styles = {
     gridTemplateRows: 'repeat(4, 1fr)',
     height: '100%',
   },
-};
+
+  buttons: {
+    width: '100%',
+  },
+
+  button: {
+    width: '50%',
+  },
+});
 
 export { styles };
